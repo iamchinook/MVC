31414e5bd0c1ee8ee1b1c2f3731d193f24b0e9e3
Chino <Chino@DESKTOP-F4R0G5L>
2022-04-22 04:57:47 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace MVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+       public ActionResult Buscar(string nombre)
+        {
+            string input = "Resultado de la busqueda: " + nombre;
+            return Content(input);
+        }
+    }
+}
